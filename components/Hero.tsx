ce0f9872245a1ece2d6a7308990199e28dbf71f9
Greekvisuals <john.andre.gulbrandsen01@gmail.com>
2025-12-07 12:26:51 +0100

    Update video source in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero: React.FC = () => {
       {/* Background Video - Above Grain (z-40) */}
       <div className="absolute inset-0 z-40">
         <video 
-          src="https://firebasestorage.googleapis.com/v0/b/redhead-productions.firebasestorage.app/o/grainshowreel%20best.mp4?alt=media&token=4a0d1186-a270-4616-aa86-7830bfb6abe3" 
+          src="https://firebasestorage.googleapis.com/v0/b/redhead-productions.firebasestorage.app/o/GRAINSHOWEEELTOPAZFAST1080p.mp4?alt=media&token=b0a3f5ed-004b-4665-9fbf-55b3f1b16e86" 
           autoPlay 
           loop 
           muted 
