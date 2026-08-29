995c69c8ce3b8f483cc4e765dc8acd2eb6cb28f9
Greekvisuals <john.andre.gulbrandsen01@gmail.com>
2026-08-29 15:15:05 +0200

    Add line break in Hero component description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -95,7 +95,8 @@ export const Hero: React.FC = () => {
             <span className="italic font-medium text-halation">set the standard.</span>
           </h2>
           <p className="mt-8 text-base md:text-xl text-white/50 max-w-2xl font-light tracking-wide leading-relaxed text-center">
-            We create visual identities people recognize, remember, and want to be part of.
+            We create visual identities people recognize, <br className="hidden md:block" />
+            remember, and want to be part of. 
           </p>
         </div>
         
